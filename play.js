fee716c2ab41d2fe77a636734148f26fc51c9e29
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-03 17:35:47 +0000

    Added input.js as a module for input function. Play.js now calls it

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 // Require the connect function from client.js
 const connect = require("./client.js");
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  process.stdout.write('.');
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+const setupInput = require("./input.js")
 
 console.log("Connecting ...");
 connect();
