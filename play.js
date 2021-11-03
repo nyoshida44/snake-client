baa8f1cad6ee86d163370606548bf158190305c4
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-03 17:21:47 +0000

    Add ctrl+c exit input to play.js

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 // Require the connect function from client.js
 const connect = require("./client.js");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  process.stdout.write('.');
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
