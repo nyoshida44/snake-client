c410301b3cdd86505de23bf5603ef7cd3121695f
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-03 18:13:27 +0000

    Snake can move in all four directions with user input. Changed input and play.js

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +15,18 @@ const handleUserInput = (key) => {
   // \u0003 maps to ctrl+c input
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");;
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
